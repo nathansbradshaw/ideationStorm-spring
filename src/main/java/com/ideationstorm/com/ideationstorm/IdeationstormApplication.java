53e90c4122aab71b4a95701b13e7fbbf1069996e
nb-midwestern <nb@buildmidwestern.com>
2022-12-22 15:02:46 -0600

    init

---
+++ src/main/java/com/ideationstorm/com/ideationstorm/IdeationstormApplication.java
@@ -0,0 +1,13 @@
+package com.ideationstorm.com.ideationstorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdeationstormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdeationstormApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ideationstorm/com/ideationstorm/IdeationstormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ideationstorm.com.ideationstorm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdeationstormApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
